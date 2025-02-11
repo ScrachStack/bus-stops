8a4e41e75ac0d66d887f897bb8497aa12ffd7636
ScrachStack <122732007+ScrachStack@users.noreply.github.com>
2025-02-11 11:10:55 -0600

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'ScrachStack'
+description 'Bus Stops'
+version '1.0.0'
+
+client_scripts {
+    'client/*',
+}
+
+server_script 'server/*'
+
+
+shared_script 'config.lua'
