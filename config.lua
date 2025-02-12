4984c7428e2ee8118b614437b828be4ee24007f4
ScrachStack <122732007+ScrachStack@users.noreply.github.com>
2025-02-12 18:05:38 +0000

    Update config.lua

--- config.lua
+++ config.lua
@@ -9,17 +9,24 @@ Settings = {}
 
 Settings.BusStops = {
     {
-        name = "1",
-        location = vector3(0,0,0),
+        name = "Sandy Shores Bus Stop",
+        location = vector3(2021.00,3814.26,31.37),
         fare = 15,
-        promptText = "You're at the 1! Pay to travel.",
+        promptText = "You're at the Sandy Shores Bus! Pay to travel.",
         blip = { id = 4, color = 3, scale = 0.8 } -- (id, color, scale)
     },
     {
-        name = "2",
-        location = vector3(0, 0, 0),
+        name = "Grove Street Bus Stop",
+        location = vector3(-94.24, -1756.20, 29.59),
         fare = 8,
-        promptText = "You're at the 2! Pay to travel.",
+        promptText = "You're at the Grove Street Bus! Pay to travel.",
+        blip = { id = 9, color = 5, scale = 0.8 } -- (id, color, scale)
+    },
+    {
+        name = "Mirror Park Bus Stop",
+        location = vector3(1043.99, -764.70, 57.78),
+        fare = 8,
+        promptText = "You're at the Mirror Park Bus! Pay to travel.",
         blip = { id = 9, color = 5, scale = 0.8 } -- (id, color, scale)
     }
 }
@@ -29,6 +36,10 @@ Settings.Messages = {
     paySuccess = "You have successfully paid for your travel.",
     payFailed = "There was an error with your payment.",
 }
+Settings.Framework = "standalone" -- Change this based on the framework being used.
+-- Options are: "standalone", "qb-core", "esx", "nd-core"
 
 Settings.TravelTime = 10 -- Time in seconds for a player to travel.
 Settings.EnableTravelCutscene = true -- [true/false]
+
+Settings.Debug = true -- [Debugging Console Spam]
